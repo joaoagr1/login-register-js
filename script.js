c24cb49944dd9eed9ea0595bc5cb6bfad369e7ee
joaoagr1 <joao.rollo1@gmail.com>
2023-05-29 18:10:37 -0300

    commit 1

---
+++ script.js
@@ -0,0 +1,50 @@
+// Objeto para armazenar os usuários registrados
+var registeredUsers = {};
+
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+  event.preventDefault(); // Impede o envio do formulário
+
+  var username = document.getElementById("loginUsername").value;
+  var password = document.getElementById("loginPassword").value;
+
+  // Verifica se o login e a senha correspondem a um usuário válido
+  if (checkCredentials(username, password)) {
+    showMessage("Login realizado com sucesso.");
+  } else {
+    showMessage("Credenciais inválidas. Tente novamente.");
+  }
+});
+
+document.getElementById("registerForm").addEventListener("submit", function(event) {
+  event.preventDefault(); // Impede o envio do formulário
+
+  var username = document.getElementById("registerUsername").value;
+  var password = document.getElementById("registerPassword").value;
+
+  // Registra um novo usuário
+  registerUser(username, password);
+});
+
+function checkCredentials(username, password) {
+  // Verifica se o login e a senha correspondem a um usuário válido
+  var user = registeredUsers[username];
+  return user && user.password === password;
+}
+
+function registerUser(username, password) {
+  // Verifica se o usuário já está registrado
+  if (registeredUsers[username]) {
+    showMessage("Usuário já registrado. Tente outro nome de usuário.");
+  } else {
+    // Registra o novo usuário
+    registeredUsers[username] = {
+      username: username,
+      password: password
+    };
+    showMessage("Novo usuário registrado com sucesso.");
+  }
+}
+
+function showMessage(message) {
+  document.getElementById("message").innerHTML = message;
+}
